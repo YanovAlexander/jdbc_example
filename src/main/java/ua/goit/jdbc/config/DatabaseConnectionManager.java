8a616c9d3456c6a73afe0aeb3ef658467282be27
Oleksandr Yanov <oleksandr.yanov@thomsonreuters.com>
2021-05-13 20:16:38 +0300

    moved to use datasource.

--- src/main/java/ua/goit/jdbc/config/DatabaseConnectionManager.java
+++ src/main/java/ua/goit/jdbc/config/DatabaseConnectionManager.java
@@ -1,32 +1,29 @@
 package ua.goit.jdbc.config;
 
+import org.postgresql.ds.PGConnectionPoolDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DatabaseConnectionManager {
-    private String url;
-    private Properties properties;
+    PGConnectionPoolDataSource pgConnectionPoolDataSource = new PGConnectionPoolDataSource();
 
     public DatabaseConnectionManager(String host, String databaseName, String username, String password) {
-        this.url = String.format("jdbc:postgresql://%s/%s", host, databaseName);
-        this.properties = enrichProperties(username, password);
+        initDataSource(host, databaseName, username, password);
     }
 
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection(url, properties);
+            return pgConnectionPoolDataSource.getConnection();
         } catch (SQLException throwables) {
             System.err.println(throwables.getMessage());
             throw new RuntimeException(throwables);
         }
     }
 
-    private Properties enrichProperties(String username, String password) {
-        Properties properties = new Properties();
-        properties.setProperty("user", username);
-        properties.setProperty("password", password);
-        return properties;
+    private void initDataSource(String host, String databaseName, String username, String password) {
+        pgConnectionPoolDataSource.setUrl(String.format("jdbc:postgresql://%s/%s", host, databaseName));
+        pgConnectionPoolDataSource.setUser(username);
+        pgConnectionPoolDataSource.setPassword(password);
     }
 }
